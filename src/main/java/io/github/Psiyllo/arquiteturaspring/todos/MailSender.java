4399fcf495ff7c15a5575c910e351bfb322bc44c
Psiyllo <paulocesaralcabral@gmail.com>
2025-07-17 13:08:30 -0300

    Criando componentes TodoValidator / MailSender (incompleto)

---
+++ src/main/java/io/github/Psiyllo/arquiteturaspring/todos/MailSender.java
@@ -0,0 +1,19 @@
+package io.github.Psiyllo.arquiteturaspring.todos;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailSender {
+
+    private TodoRepository repository;
+
+    public MailSender(TodoRepository repository) {
+        this.repository = repository;
+    }
+
+    public void enviar (String mensagem){
+        System.out.println("Enviando email: " + mensagem);
+
+    }
+
+}
