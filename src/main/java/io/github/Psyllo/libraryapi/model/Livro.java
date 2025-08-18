95689d1d957b670a11d290aae3fd4b70ff7b93e4
Psiyllo <paulocesaralcabral@gmail.com>
2025-08-18 12:58:55 -0300

    Mapeando entidade 'Livro'(incompleto)

---
+++ src/main/java/io/github/Psyllo/libraryapi/model/Livro.java
@@ -0,0 +1,40 @@
+package io.github.Psyllo.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Getter
+@Setter
+public class Livro {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    @Column(name = "data_publicacao", nullable = false)
+    private LocalDate dataPublicacao;
+
+    @Column(name = "genero", length = 30, nullable = false)
+    private String genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private Double preco;
+//    private BigDecimal preco;       BigDecimal melhor para valores monetários
+
+    @ManyToOne
+    @JoinColumn (name = "id_autor")
+    private Autor autor;
+
+}
