4f0835d2d48aaef94dd3dc4e5e9ecb85f38f3fe7
Psiyllo <paulocesaralcabral@gmail.com>
2025-08-22 09:34:12 -0300

    Entendendo sobre aplicações de Fetch(Lazy,Eager) e suas peculiaridades em relacionamentos @ManyToOne

--- src/test/java/io/github/Psyllo/libraryapi/repository/LivroRepositoryTest.java
+++ src/test/java/io/github/Psyllo/libraryapi/repository/LivroRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cglib.core.Local;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -22,13 +23,13 @@ class LivroRepositoryTest {
     AutorRepository autorRepository;
 
     @Test
-    void salvarTest(){
+    void salvarTest() {
         Livro livro = new Livro();
         livro.setIsbn("901212-23213");
         livro.setPreco(BigDecimal.valueOf(200));
         livro.setTitulo("LOM");
         livro.setGenero(GeneroLivro.MISTERIO);
-        livro.setDataPublicacao(LocalDate.of(2013,8,28));
+        livro.setDataPublicacao(LocalDate.of(2013, 8, 28));
 
         Autor autor = autorRepository
                 .findById(UUID.fromString("612668f2-0577-4202-a902-cab7775338ca"))
@@ -40,18 +41,18 @@ class LivroRepositoryTest {
     }
 
     @Test
-    void salvarAutorELivroTest(){
+    void salvarAutorELivroTest() {
         Livro livro = new Livro();
         livro.setIsbn("901212-23213");
         livro.setPreco(BigDecimal.valueOf(200));
         livro.setTitulo("Cientifica top");
         livro.setGenero(GeneroLivro.CIENCIA);
-        livro.setDataPublicacao(LocalDate.of(2013,8,28));
+        livro.setDataPublicacao(LocalDate.of(2013, 8, 28));
 
         Autor autor = new Autor();
         autor.setNome("rafael");
         autor.setNacionalidade("Brasileiro");
-        autor.setDataNascimento(LocalDate.of(2001,6,14));
+        autor.setDataNascimento(LocalDate.of(2001, 6, 14));
 
         autorRepository.save(autor);
 
@@ -59,19 +60,20 @@ class LivroRepositoryTest {
 
         repository.save(livro);
     }
+
     @Test
-    void salvarCascadeTest(){
+    void salvarCascadeTest() {
         Livro livro = new Livro();
         livro.setIsbn("901212-23213");
         livro.setPreco(BigDecimal.valueOf(200));
         livro.setTitulo("LOM");
         livro.setGenero(GeneroLivro.MISTERIO);
-        livro.setDataPublicacao(LocalDate.of(2013,8,28));
+        livro.setDataPublicacao(LocalDate.of(2013, 8, 28));
 
         Autor autor = new Autor();
         autor.setNome("Paulo");
         autor.setNacionalidade("Brasileiro");
-        autor.setDataNascimento(LocalDate.of(2004,6,14));
+        autor.setDataNascimento(LocalDate.of(2004, 6, 14));
 
         livro.setAutor(autor);
 
@@ -79,7 +81,7 @@ class LivroRepositoryTest {
     }
 
     @Test
-    void atualizarAutorDoLivro(){
+    void atualizarAutorDoLivro() {
         UUID id = UUID.fromString("ba9ec9c8-eb5d-40f5-8bf9-7815a6a8faca");
         var livroParaAtualizar = repository.findById(id).orElse(null);
 
@@ -92,15 +94,27 @@ class LivroRepositoryTest {
     }
 
     @Test
-    void deletar(){
+    void deletar() {
         UUID id = UUID.fromString("ba9ec9c8-eb5d-40f5-8bf9-7815a6a8faca");
         repository.deleteById(id);
     }
 
     @Test
-    void deletarCascade(){
+    void deletarCascade() {
         UUID id = UUID.fromString("ba9ec9c8-eb5d-40f5-8bf9-7815a6a8faca");
         repository.deleteById(id);
         //Se habilitar o cascade na model isso ira deletar o livro e também o autor do livro.
+    }
+
+    @Test
+    @Transactional
+    void buscarLivroTest() {
+        UUID id = UUID.fromString("075888f2-2265-43bc-9d7b-e2fe0f24b6c3");
+        Livro livro = repository.findById(id).orElse(null);
+
+        System.out.println("Livro: ");
+        System.out.println(livro.getTitulo());
+        System.out.println("Autor :");
+        System.out.println(livro.getAutor().getNome());
     }
 }
