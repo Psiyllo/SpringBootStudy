ebaf3445cb0f8981269ca5f8d52c23ae58e1fff7
Psiyllo <paulocesaralcabral@gmail.com>
2025-08-27 09:13:03 -0300

    Consultas simples com query methods (LivroRepository)

--- src/test/java/io/github/Psyllo/libraryapi/repository/LivroRepositoryTest.java
+++ src/test/java/io/github/Psyllo/libraryapi/repository/LivroRepositoryTest.java
@@ -12,13 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @SpringBootTest
 class LivroRepositoryTest {
 
     @Autowired
-    LivroRepository repository;
+    LivroRepository livroRepository;
 
     @Autowired
     AutorRepository autorRepository;
@@ -38,7 +39,7 @@ class LivroRepositoryTest {
 
         livro.setAutor(autor);
 
-        repository.save(livro);
+        livroRepository.save(livro);
     }
 
     @Test
@@ -59,7 +60,7 @@ class LivroRepositoryTest {
 
         livro.setAutor(autor);
 
-        repository.save(livro);
+        livroRepository.save(livro);
     }
 
     @Test
@@ -78,44 +79,77 @@ class LivroRepositoryTest {
 
         livro.setAutor(autor);
 
-        repository.save(livro);
+        livroRepository.save(livro);
     }
 
     @Test
     void atualizarAutorDoLivro() {
         UUID id = UUID.fromString("ba9ec9c8-eb5d-40f5-8bf9-7815a6a8faca");
-        var livroParaAtualizar = repository.findById(id).orElse(null);
+        var livroParaAtualizar = livroRepository.findById(id).orElse(null);
 
         UUID idAutor = UUID.fromString("612668f2-0577-4202-a902-cab7775338ca");
         Autor Eduarda = autorRepository.findById(idAutor).orElse(null);
 
         livroParaAtualizar.setAutor(Eduarda);
 
-        repository.save(livroParaAtualizar);
+        livroRepository.save(livroParaAtualizar);
     }
 
     @Test
     void deletar() {
         UUID id = UUID.fromString("ba9ec9c8-eb5d-40f5-8bf9-7815a6a8faca");
-        repository.deleteById(id);
+        livroRepository.deleteById(id);
     }
 
     @Test
     void deletarCascade() {
         UUID id = UUID.fromString("ba9ec9c8-eb5d-40f5-8bf9-7815a6a8faca");
-        repository.deleteById(id);
+        livroRepository.deleteById(id);
         //Se habilitar o cascade na model isso ira deletar o livro e também o autor do livro.
     }
 
     @Test
     @Transactional
     void buscarLivroTest() {
-        UUID id = UUID.fromString("075888f2-2265-43bc-9d7b-e2fe0f24b6c3");
-        Livro livro = repository.findById(id).orElse(null);
+        UUID id = UUID.fromString("84e65d7d-00be-4ccc-9284-9e1e4fec39db");
+        Livro livro = livroRepository.findById(id).orElse(null);
 
         System.out.println("Livro: ");
         System.out.println(livro.getTitulo());
         System.out.println("Autor :");
         System.out.println(livro.getAutor().getNome());
     }
+
+    @Test
+    void listarLivrosAutor(){
+        var id = UUID.fromString("3407bc7c-5b7b-40a1-9407-ff1453f6f645");
+        var autor = autorRepository.findById(id).get();
+
+        List<Livro> livrosListar = livroRepository.findByAutor(autor);
+        autor.setLivros(livrosListar);
+
+        autor.getLivros().forEach(System.out::println);
+    }
+
+    @Test
+    void listarLivrosTitulo(){
+        List<Livro> lista = livroRepository.findByTitulo("genshin impact lixo");
+        lista.forEach(System.out::println);
+    }
+
+    @Test
+    void listarLivrosIsbn(){
+        List<Livro> listaIsbn = livroRepository.findByIsbn("415342-93243");
+        listaIsbn.forEach(System.out::println);
+    }
+
+    @Test
+    void listarLivrosTituloAndPreco(){
+        var preco = BigDecimal.valueOf(2.00);
+        var titulo = "genshin impact lixo";
+
+        List<Livro> listaTituloAndPreco = livroRepository.findByTituloAndPreco(titulo, preco);
+        listaTituloAndPreco.forEach(System.out::println);
+    }
+
 }
