b320d8c1f7cb3ad8071efc2db808fb5101a4a17f
Psiyllo <paulocesaralcabral@gmail.com>
2025-08-27 11:33:15 -0300

    Trabalhando outras formas de se utilizar consultar JPQL com anotação @Query

--- src/main/java/io/github/Psyllo/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/Psyllo/libraryapi/repository/LivroRepository.java
@@ -3,12 +3,16 @@ package io.github.Psyllo.libraryapi.repository;
 import io.github.Psyllo.libraryapi.model.Autor;
 import io.github.Psyllo.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * @see LivroRepositoryTest
+ */
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
 
     //Query Method
@@ -26,4 +30,32 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
 
     //select * from livro where titulo like= ?
     List<Livro> findByTituloLike(String Titulo);
+
+    //JPQL referencia entidade e propriedade
+    //select l.* from Livro as l order by l.titulo, l.preco
+    @Query(" select l from Livro as l order by l.titulo, l.preco ")
+    List<Livro> listarTodosLivrosOrdenadosTituloAndPreco();
+
+    //select a.*, l.titulo from livro l join autor a on a.id = l.id_autor
+    @Query(" select a from Livro l join l.autor a")
+    List<Autor> listarAutoresDosLivros();
+
+    @Query(" select distinct l.titulo from Livro l order by l.titulo")
+    List<String> listarLivrosDiferentes();
+
+    @Query("""
+            select distinct l.genero from Livro l
+            join l.autor a
+            where a.nacionalidade = 'Brasileira'
+            order by l.genero
+    """)
+    List<String> listarGeneroQuandoNacionalidadeBrasileira();
+
+    public interface LivroTituloPreco{
+        String getTitulo();
+        BigDecimal getPreco();
+    }
+
+    @Query(" select l.titulo, l.preco from Livro l order by l.titulo")
+    List<LivroTituloPreco> listarTituloAndPreco();
 }
