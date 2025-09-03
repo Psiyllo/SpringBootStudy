7ce63068e78742dfd6bbd4f7259f48bff8f202cb
Psiyllo <paulocesaralcabral@gmail.com>
2025-09-03 09:40:53 -0300

    Anotando @Transaction no service para boas práticas

--- src/main/java/io/github/Psyllo/libraryapi/service/AutorService.java
+++ src/main/java/io/github/Psyllo/libraryapi/service/AutorService.java
@@ -3,6 +3,7 @@ package io.github.Psyllo.libraryapi.service;
 import io.github.Psyllo.libraryapi.model.Autor;
 import io.github.Psyllo.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -16,14 +17,17 @@ public class AutorService {
         this.repository = repository;
     }
 
+    @Transactional
     public Autor salvar (Autor autor){
         return repository.save(autor);
     }
 
+    @Transactional(readOnly = true)
     public Optional<Autor> obterPorId(UUID id){
         return repository.findById(id);
     }
 
+    @Transactional
     public void deletar(Autor autor){
         repository.delete(autor);
     }
