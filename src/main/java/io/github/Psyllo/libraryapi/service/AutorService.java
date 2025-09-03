84c58d43df449c4edc31335f2ec9619eb9aa9558
Psiyllo <paulocesaralcabral@gmail.com>
2025-09-03 10:39:56 -0300

    Adicionando pesquisarAutor (AutorController)

--- src/main/java/io/github/Psyllo/libraryapi/service/AutorService.java
+++ src/main/java/io/github/Psyllo/libraryapi/service/AutorService.java
@@ -5,6 +5,7 @@ import io.github.Psyllo.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -30,5 +31,19 @@ public class AutorService {
     @Transactional
     public void deletar(Autor autor){
         repository.delete(autor);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Autor> filtrarAutor (String nome, String nacionalidade){
+        if(nome != null && nacionalidade != null){
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+        if(nome != null){
+            return repository.findByNome(nome);
+        }
+        if(nacionalidade != null){
+            return repository.findByNacionalidade(nacionalidade);
+        }
+        return repository.findAll();
     }
 }
