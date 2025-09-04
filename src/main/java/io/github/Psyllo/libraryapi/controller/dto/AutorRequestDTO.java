9ece6a35d9d0f58925dc6b1d91f8d4e1089069e7
Psiyllo <paulocesaralcabral@gmail.com>
2025-09-04 11:01:42 -0300

    Adicionando validação de campos com Bean Validation (DTO's)

--- src/main/java/io/github/Psyllo/libraryapi/controller/dto/AutorRequestDTO.java
+++ src/main/java/io/github/Psyllo/libraryapi/controller/dto/AutorRequestDTO.java
@@ -1,12 +1,17 @@
 package io.github.Psyllo.libraryapi.controller.dto;
 
 import io.github.Psyllo.libraryapi.model.Autor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record AutorRequestDTO(
+        @NotBlank(message = "Campo Obrigatório")
         String nome,
+        @NotNull(message = "Campo Obrigatório")
         LocalDate dataNascimento,
+        @NotBlank(message = "Campo Obrigatório")
         String nacionalidade) {
 
     public Autor mapearParaAutor(){
