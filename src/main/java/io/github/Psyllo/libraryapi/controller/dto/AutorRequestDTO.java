8467771c9b921567f40e2e470cb29d3d89da0579
Psiyllo <paulocesaralcabral@gmail.com>
2025-09-09 12:47:13 -0300

    Feat: Adicionando validações @Size @Past no AutorRequestDTO

--- src/main/java/io/github/Psyllo/libraryapi/controller/dto/AutorRequestDTO.java
+++ src/main/java/io/github/Psyllo/libraryapi/controller/dto/AutorRequestDTO.java
@@ -3,15 +3,20 @@ package io.github.Psyllo.libraryapi.controller.dto;
 import io.github.Psyllo.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record AutorRequestDTO(
         @NotBlank(message = "Campo Obrigatório")
+        @Size(min = 2, max = 100, message = "Campo fora do tamanho padrão")
         String nome,
         @NotNull(message = "Campo Obrigatório")
+        @Past(message = "Não pode ser uma data futura")
         LocalDate dataNascimento,
         @NotBlank(message = "Campo Obrigatório")
+        @Size(min = 2, max = 50, message = "Campo fora do tamanho padrão")
         String nacionalidade) {
 
     public Autor mapearParaAutor(){
--- src/main/java/io/github/Psyllo/libraryapi/controller/dto/AutorResponseDTO.java
+++ src/main/java/io/github/Psyllo/libraryapi/controller/dto/AutorResponseDTO.java
@@ -2,16 +2,14 @@ package io.github.Psyllo.libraryapi.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record AutorResponseDTO(
         UUID id,
-        @NotBlank(message = "Campo Obrigatório")
         String nome,
-        @NotNull(message = "Campo Obrigatório")
         LocalDate dataNascimento,
-        @NotBlank(message = "Campo Obrigatório")
         String nacionalidade) {
 }
