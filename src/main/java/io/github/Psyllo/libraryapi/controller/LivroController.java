9f405c41157c71f44076255d9d7275f284b388e3
Psiyllo <paulocesaralcabral@gmail.com>
2025-10-08 11:06:33 -0300

    Feat: Criando interface que gera URI(GenericController) para implementar nos controllers de Autor e Livro

--- src/main/java/io/github/Psyllo/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/Psyllo/libraryapi/controller/LivroController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("livros")
-public class LivroController {
+public class LivroController implements GenericController{
 
     private final LivroService service;
     private final LivroMapper mapper;
@@ -27,9 +27,8 @@ public class LivroController {
         try{
             Livro livro = mapper.toEntity(dto);
             service.salvar(livro);
-
-
-            return ResponseEntity.ok(livro);
+            var url = gerarHeaderLocation(livro.getId());
+            return ResponseEntity.created(url).build();
         } catch (RegistroDuplicadoException e){
             var erroDTO = ErroResposta.conflito(e.getMessage());
             return ResponseEntity.status(erroDTO.status()).body(erroDTO);
