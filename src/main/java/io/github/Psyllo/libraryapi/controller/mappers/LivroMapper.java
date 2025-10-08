aae039bef57b32fc9bf9df27f891b03e0d6b4849
Psiyllo <paulocesaralcabral@gmail.com>
2025-10-08 13:18:46 -0300

    Feat: Adicionando método 'obterDetalhes' e usando composição de mappers (AutorMapper e LivroMapper) com 'uses = {}' do MapStruct
    .git/COMMIT_EDITMSG[+] [unix] (13:24 08/10/2025)                                                                                                        2,0-1 Top
    Type  :qa!  and press <Enter> to abandon all changes and exit Vim

--- src/main/java/io/github/Psyllo/libraryapi/controller/mappers/LivroMapper.java
+++ src/main/java/io/github/Psyllo/libraryapi/controller/mappers/LivroMapper.java
@@ -1,13 +1,14 @@
 package io.github.Psyllo.libraryapi.controller.mappers;
 
 import io.github.Psyllo.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.Psyllo.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import io.github.Psyllo.libraryapi.model.Livro;
 import io.github.Psyllo.libraryapi.repository.AutorRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {AutorMapper.class})
 public abstract class LivroMapper {
 
     @Autowired
@@ -16,4 +17,5 @@ public abstract class LivroMapper {
     @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null))")
     public abstract Livro toEntity(CadastroLivroDTO dto);
 
+    public abstract ResultadoPesquisaLivroDTO toDTO(Livro livro);
 }
