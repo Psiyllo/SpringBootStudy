096aede2c06f4bd8e09c913a7a146dab71f7bcce
Psiyllo <paulocesaralcabral@gmail.com>
2025-10-09 17:52:23 -0300

    feat: implemetando cadastro de usuarios

---
+++ target/generated-sources/annotations/io/github/Psyllo/libraryapi/controller/mappers/UsuarioMapperImpl.java
@@ -0,0 +1,58 @@
+package io.github.Psyllo.libraryapi.controller.mappers;
+
+import io.github.Psyllo.libraryapi.controller.dto.UsuarioRequestDTO;
+import io.github.Psyllo.libraryapi.controller.dto.UsuarioResponseDTO;
+import io.github.Psyllo.libraryapi.model.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-10-09T16:45:36-0300",
+    comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
+)
+@Component
+public class UsuarioMapperImpl implements UsuarioMapper {
+
+    @Override
+    public Usuario toEntity(UsuarioRequestDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Usuario usuario = new Usuario();
+
+        usuario.setLogin( dto.login() );
+        usuario.setSenha( dto.senha() );
+        List<String> list = dto.roles();
+        if ( list != null ) {
+            usuario.setRoles( new ArrayList<String>( list ) );
+        }
+
+        return usuario;
+    }
+
+    @Override
+    public UsuarioResponseDTO toDto(Usuario entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        String login = null;
+        String senha = null;
+        List<String> roles = null;
+
+        login = entity.getLogin();
+        senha = entity.getSenha();
+        List<String> list = entity.getRoles();
+        if ( list != null ) {
+            roles = new ArrayList<String>( list );
+        }
+
+        UsuarioResponseDTO usuarioResponseDTO = new UsuarioResponseDTO( login, senha, roles );
+
+        return usuarioResponseDTO;
+    }
+}
