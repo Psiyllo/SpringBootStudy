1555e1cfdda1f528fc5563c33bf22483614e54ab
Psiyllo <paulocesaralcabral@gmail.com>
2025-10-09 19:18:48 -0300

    feat: implemetando auditoria de usuarios

---
+++ src/main/java/io/github/Psyllo/libraryapi/Security/SecurityService.java
@@ -0,0 +1,24 @@
+package io.github.Psyllo.libraryapi.Security;
+
+import io.github.Psyllo.libraryapi.model.Usuario;
+import io.github.Psyllo.libraryapi.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityService {
+
+    private final UsuarioService usuarioService;
+
+    public Usuario obterUsuarioLogado(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String login = userDetails.getUsername();
+        return usuarioService.obterPorLogin(login);
+    }
+
+}
--- target/generated-sources/annotations/io/github/Psyllo/libraryapi/controller/mappers/LivroMapperImpl.java
+++ target/generated-sources/annotations/io/github/Psyllo/libraryapi/controller/mappers/LivroMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-09T16:45:36-0300",
+    date = "2025-10-09T19:06:13-0300",
     comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
 )
 @Component
