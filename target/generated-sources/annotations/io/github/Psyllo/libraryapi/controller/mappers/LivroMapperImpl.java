67d13ca7901c4652d17cf3e9720636a8078c083a
Psiyllo <paulocesaralcabral@gmail.com>
2025-10-14 13:06:43 -0300

    fix: remove .env from repository

---
+++ target/generated-sources/annotations/io/github/Psyllo/libraryapi/controller/mappers/LivroMapperImpl.java
@@ -0,0 +1,71 @@
+package io.github.Psyllo.libraryapi.controller.mappers;
+
+import io.github.Psyllo.libraryapi.controller.dto.AutorResponseDTO;
+import io.github.Psyllo.libraryapi.controller.dto.LivroRequestDTO;
+import io.github.Psyllo.libraryapi.controller.dto.LivroResponseDTO;
+import io.github.Psyllo.libraryapi.model.GeneroLivro;
+import io.github.Psyllo.libraryapi.model.Livro;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-10-14T13:00:53-0300",
+    comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
+)
+@Component
+public class LivroMapperImpl extends LivroMapper {
+
+    @Autowired
+    private AutorMapper autorMapper;
+
+    @Override
+    public Livro toEntity(LivroRequestDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Livro livro = new Livro();
+
+        livro.setIsbn( dto.isbn() );
+        livro.setTitulo( dto.titulo() );
+        livro.setDataPublicacao( dto.dataPublicacao() );
+        livro.setGenero( dto.genero() );
+        livro.setPreco( dto.preco() );
+
+        livro.setAutor( autorRepository.findById(dto.idAutor()).orElse(null) );
+
+        return livro;
+    }
+
+    @Override
+    public LivroResponseDTO toDTO(Livro livro) {
+        if ( livro == null ) {
+            return null;
+        }
+
+        UUID id = null;
+        String isbn = null;
+        String titulo = null;
+        LocalDate dataPublicacao = null;
+        GeneroLivro genero = null;
+        BigDecimal preco = null;
+        AutorResponseDTO autor = null;
+
+        id = livro.getId();
+        isbn = livro.getIsbn();
+        titulo = livro.getTitulo();
+        dataPublicacao = livro.getDataPublicacao();
+        genero = livro.getGenero();
+        preco = livro.getPreco();
+        autor = autorMapper.toDTO( livro.getAutor() );
+
+        LivroResponseDTO livroResponseDTO = new LivroResponseDTO( id, isbn, titulo, dataPublicacao, genero, preco, autor );
+
+        return livroResponseDTO;
+    }
+}
---
+++ target/generated-sources/annotations/io/github/Psyllo/libraryapi/controller/mappers/UsuarioMapperImpl.java
@@ -0,0 +1,61 @@
+package io.github.Psyllo.libraryapi.controller.mappers;
+
+import io.github.Psyllo.libraryapi.controller.dto.UsuarioRequestDTO;
+import io.github.Psyllo.libraryapi.controller.dto.UsuarioResponseDTO;
+import io.github.Psyllo.libraryapi.model.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-10-14T13:00:53-0300",
+    comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
+)
+@Component
+public class UsuarioMapperImpl implements UsuarioMapper {
+
+    @Override
+    public Usuario toEntity(UsuarioRequestDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Usuario usuario = new Usuario();
+
+        usuario.setLogin( dto.login() );
+        usuario.setSenha( dto.senha() );
+        usuario.setEmail( dto.email() );
+        List<String> list = dto.roles();
+        if ( list != null ) {
+            usuario.setRoles( new ArrayList<String>( list ) );
+        }
+
+        return usuario;
+    }
+
+    @Override
+    public UsuarioResponseDTO toDto(Usuario entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        String login = null;
+        String email = null;
+        String senha = null;
+        List<String> roles = null;
+
+        login = entity.getLogin();
+        email = entity.getEmail();
+        senha = entity.getSenha();
+        List<String> list = entity.getRoles();
+        if ( list != null ) {
+            roles = new ArrayList<String>( list );
+        }
+
+        UsuarioResponseDTO usuarioResponseDTO = new UsuarioResponseDTO( login, email, senha, roles );
+
+        return usuarioResponseDTO;
+    }
+}
